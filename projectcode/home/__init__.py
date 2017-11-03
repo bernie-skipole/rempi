31374e8d8994608de30ca1276792695e8bd025ec
skipole <42965323+bernie-skipole@users.noreply.github.com>
2017-11-03 00:14:04 +0000

    adding widgets with device name to index page

--- projectcode/home/__init__.py
+++ projectcode/home/__init__.py
@@ -4,12 +4,16 @@
 from ... import FailPage, GoTo, ValidateError, ServerError
 
 
-from .. import database_ops
+from .. import database_ops, hardware
 
 
 
 def index_page(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
     "Fills in the public index page"
+
+    page_data['headingtext', 'large_text'] = hardware.get_name()
+
+    page_data['intro', 'large_text'] = "You are connected to Raspberry Pi - " + hardware.get_name()
 
     message_string = database_ops.get_all_messages()
     if message_string:
