fad56d22ea7c6f2bb8ec3998e9f32a1e4ca7b5da
skipole <42965323+bernie-skipole@users.noreply.github.com>
2018-11-30 11:40:41 +0000

    working on removing database

--- projectcode/login/__init__.py
+++ projectcode/login/__init__.py
@@ -4,27 +4,25 @@ from base64 import b64decode
 
 from ... import FailPage, GoTo, ValidateError, ServerError
 
-from .. import database_ops
+
+# This is the access username
+_USERNAME = "admin"
+# This is the access password
+_PASSWORD = "password"
 
 
 def check_login(environ):
     "Returns True if login ok, False otherwise"
     try:
-        access_user = database_ops.get_access_user()
-        access_password, seed = database_ops.get_password(access_user)
-        # access_password is the hashed password stored in the database
-        if not access_password:
-            return False
         auth = environ.get('HTTP_AUTHORIZATION')
         if auth:
             scheme, data = auth.split(" ", 1)
             if scheme.lower() != 'basic':
                 return False
             username, password = b64decode(data).decode('UTF-8').split(':', 1)
-            if username != access_user:
+            if username != _USERNAME:
                 return False
-            hashed_password, seed = database_ops.hash_password(password, seed)
-            if hashed_password == access_password:
+            if password == _PASSWORD:
                 # login ok
                 return True
     except:
@@ -32,22 +30,6 @@ def check_login(environ):
         # Any exception causes False to be returned
     # login fail
     return False
-
-
-def check_password(password):
-    "Returns True if password ok, False otherwise"
-    try:
-        access_user = database_ops.get_access_user()
-        database_password, seed = database_ops.get_password(access_user)
-        if (database_password, seed) == database_ops.hash_password(password, seed):
-            # password ok
-            return True
-    except:
-        pass
-        # Any exception causes False to be returned
-    # password fail
-    return False
-
 
 
 def request_login(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
