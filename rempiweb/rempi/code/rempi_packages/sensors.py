20f78f15b3d7b950920b2c2b3c8142803e172929
skipole <42965323+bernie-skipole@users.noreply.github.com>
2019-04-03 12:19:08 +0100

    rempiweb being changed to new system

--- rempiweb/rempi/code/rempi_packages/sensors.py
+++ rempiweb/rempi/code/rempi_packages/sensors.py
@@ -7,44 +7,41 @@ from .. import hardware
 
 def sensor_table(skicall):
     """sets three lists for sensor table into page data"""
-    sensors = hardware.get_input_names()
+    sensors = ["LED", "TEMPERATURE"]
     skicall.page_data['sensors', 'col1'] = sensors
-    skicall.page_data['sensors', 'col2'] = _get_sensor_values(sensors)
-    skicall.page_data['sensors', 'col3'] = [hardware.get_input_description(name) for name in sensors]
+    skicall.page_data['sensors', 'col2'] = _get_sensor_values(sensors, skicall)
+    skicall.page_data['sensors', 'col3'] = ["LED attached to pi", "Temperature from probe"]
 
 
 def sensors_json_api(skicall):
     "Returns sensors dictionary"
-    sensors = hardware.get_input_names()
-    values = _get_sensor_values(sensors)
+    sensors = ["LED", "TEMPERATURE"]
+    values = _get_sensor_values(sensors, skicall)
     return collections.OrderedDict(zip(sensors,values))
 
 
-def _get_sensor_values(sensors):
+def _get_sensor_values(sensors, skicall):
     "Returns list of sensor values"
+    redis = skicall.proj_data['redis']
     values = []
     for name in sensors:
-        input_type = hardware.get_input_type(name)
-        if input_type == 'boolean':
-            invalue = hardware.get_boolean_input(name)
-            if invalue is None:
-                value = 'UNKNOWN'
-            elif invalue:
+        if name == "LED":
+            # get led status from redis
+            led_status = redis.get('led')
+            if led_status == b"ON":
                 value = 'ON'
             else:
                 value = 'OFF'
-        elif input_type == 'text':
-            value = hardware.get_text_input(name)
-        elif input_type == 'float':
-            value = str(hardware.get_float_input(name))
-        else:
-            value = ''
-        values.append(value)
+            values.append(value)
+        if name == "TEMPERATURE":
+            value = redis.get('temperature').decode("utf-8")
+            values.append(value)
     return values
 
 
 def temperature(skicall):
     "Fills weather station temperature meter"
-    temperature = str(hardware.get_temperature())
+    redis = skicall.proj_data['redis']
+    temperature = redis.get('temperature').decode("utf-8")
     skicall.page_data["temperature", "measurement"] = temperature
     skicall.page_data["temperature_value", "text"] = "Temperature : %s" % (temperature,)
