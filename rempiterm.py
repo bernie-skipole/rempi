30b671a7839fcafc9024fb3f2100d59587ac2832
skipole <42965323+bernie-skipole@users.noreply.github.com>
2019-04-05 15:05:54 +0100

    simplified rempiterm - now a single rempiterm.py script

---
+++ rempiterm.py
@@ -0,0 +1,111 @@
+# Python3 program to display a terminal status and control menu
+# for the Astronomy Centre REMSCOPE
+
+import sys, curses
+
+from redis import StrictRedis
+
+
+
+def main(stdscr, redis, pubsub):
+    "Sets up the windows, and checks for input"
+
+    # no blinking cursor
+    curses.curs_set(False)
+
+    # getch will block for just half a second, allowing status to be checked
+    curses.halfdelay(5)
+
+    # set color pairs
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_GREEN)
+    curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_WHITE)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLUE)
+
+    # standard screen shows as a green border around the status and menu windows
+    stdscr.bkgd(' ', curses.color_pair(1))
+    stdscr.clear()
+    stdscr.refresh()
+
+    status_window_height = 9
+
+    # start status window, with colour pair 2
+    statuswin = curses.newwin(status_window_height, curses.COLS-2, 1, 1)
+    statuswin.bkgd(' ', curses.color_pair(2))
+    showstatus(statuswin, redis, "Terminal started")
+
+    # start menu1 window, with colour pair 3
+    # the height is full screen height less the status window height and less 2 for the border
+    menu1win = curses.newwin(curses.LINES-status_window_height-2, curses.COLS-2, status_window_height+1, 1)
+    menu1win.bkgd(' ', curses.color_pair(3))
+
+    # menu1 options
+    menu1win.clear()
+    menu1win.box(0,0)
+    menu1win.addstr(2,5,"Choose an option:")
+    menu1win.addstr(4,10,"1 - To turn on the LED")
+    menu1win.addstr(5,10,"2 - To turn off the LED")
+    menu1win.addstr(9,10,"Q - To quit the program")
+    menu1win.noutrefresh()
+
+    curses.doupdate()
+
+    # wait for an input
+    while True:
+
+        c = stdscr.getch()
+        # For each menu option
+        if c == ord('1'):
+            showstatus(statuswin, redis, "LED ON request")
+            redis.publish("control02", "ON")
+            curses.doupdate()
+            continue
+        elif c == ord('2'):
+            showstatus(statuswin, redis, "LED OFF request")
+            redis.publish("control02", "OFF")
+            curses.doupdate()
+            continue
+        # Final menu option q for quit
+        elif c == ord('q') or c == ord('Q'):
+            break  # Exit the while loop
+
+        # has a message been received by redis pubsub?
+        message = pubsub.get_message()
+        if message:
+            # Yes it has, so show it as a status
+            status_message = "Message received: " + message['data'].decode("utf-8")
+            showstatus(statuswin, redis, status_message)
+            curses.doupdate()
+
+    # Clear screen and end the program
+    stdscr.clear()
+    stdscr.refresh()
+
+
+def showstatus(statuswin, redis, status_message):
+    "fills in the status window"
+    # get led status locally
+    if redis.get('led') == b"ON":
+        led_status = "LED: ON"
+    else:
+        led_status = "LED: OFF"
+    statuswin.clear()
+    statuswin.box(0,0)
+    statuswin.addstr(2,5,"REMScope Status:")
+    statuswin.addstr(3,10,status_message)
+    statuswin.addstr(4,10,led_status)
+    statuswin.noutrefresh()
+
+
+if __name__ == "__main__":
+
+
+    # create redis connection
+    redis = StrictRedis(host='localhost', port=6379)
+
+    pubsub = redis.pubsub(ignore_subscribe_messages=True)
+    pubsub.subscribe('alert02')
+
+    # start the curses screen
+    curses.wrapper(main, redis, pubsub)
+
+
