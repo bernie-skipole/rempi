9f8c460305fba992bff295852ead622d0ef9f6da
bernie-skipole <bernie@skipole.co.uk>
2020-05-15 17:41:35 +0100

    working on telescope control

--- rempicontrol/control/telescope.py
+++ rempicontrol/control/telescope.py
@@ -60,15 +60,61 @@ class Telescope(object):
         self.alt = 0.0           # initial conditions, could be changed to a 'parking' state
         self.az = 0.0
         self.target_name = ''
-        self.ra = None
-        self.dec = None
-        self.rconn.delete("rempi01_target_name")
-        self.rconn.delete("rempi01_target_ra")
-        self.rconn.delete("rempi01_target_dec")
+        self.ra = ''
+        self.dec = ''
 
         # the telescope motors
         self.motor1 = motors.Motor('motor1',rconn)
         self.motor2 = motors.Motor('motor2',rconn)
+
+
+    @property
+    def target_name(self):
+        "Returns target name or empty string if name not set"
+        target_name = self.rconn.get("rempi01_target_name")
+        if target_name is None:
+            return ''
+        return target_name.decode("utf-8")
+
+    @target_name.setter
+    def target_name(self, target_name):
+        self.rconn.set("rempi01_target_name", target_name)
+
+    @target_name.deleter
+    def target_name(self):
+        self.rconn.delete("rempi01_target_name")
+
+    @property
+    def ra(self):
+        "Returns target ra as a string or empty string if ra not set"
+        target_ra = self.rconn.get("rempi01_target_ra")
+        if target_ra is None:
+            return ''
+        return target_ra.decode("utf-8")
+
+    @ra.setter
+    def ra(self, target_ra):
+        self.rconn.set("rempi01_target_ra", target_ra)
+
+    @ra.deleter
+    def ra(self):
+        self.rconn.delete("rempi01_target_ra")
+
+    @property
+    def dec(self):
+        "Returns target dec as a string or empty string if dec not set"
+        target_dec = self.rconn.get("rempi01_target_dec")
+        if target_dec is None:
+            return ''
+        return target_dec.decode("utf-8")
+
+    @dec.setter
+    def dec(self, target_dec):
+        self.rconn.set("rempi01_target_dec", target_dec)
+
+    @dec.deleter
+    def dec(self):
+        self.rconn.delete("rempi01_target_dec")
 
 
     def goto(self, msg):
@@ -76,12 +122,12 @@ class Telescope(object):
         # Set tracking False, to stop previous tracking
         self.tracking = False
         # positions consists of target_name, ra, dec and 20 sets of timestamp,alt,az which is a string and 62 floats,
-        # these alt and az values are for ten minutes (twenty half minutes), every 30 seconds
+        # these alt and az values are for nineteen 30 second intervals (9.5 minutes), these are updated from the server every 4 minutes
         positions = unpack("10s"+"d"*62, msg['data'])
         self.target_name = positions[0].rstrip(b'\x00').decode("utf-8")
         self.ra = positions[1]
         self.dec = positions[2]
-        logging.info('Goto received RA %s DEC %s' % (self.ra, self.dec))
+        logging.info('Goto received RA %s DEC %s' % (positions[1], positions[2]))
         # create a dictionary of {timestamp:(alt,az), timestamp:(alt,az),....}
         # note positions[3:] is used as the first three elements are the name, ra and dec
         time_altaz = { tstmp : (altdeg, azdeg) for tstmp, altdeg, azdeg in zip(*[iter(positions[3:])]*3) }
@@ -96,30 +142,77 @@ class Telescope(object):
  
 
     def createcurves(self, time_altaz):
-        "create four sets of curves, each with a timestamp covering the 20 sets of timestamps"
+        """create four sets of curves, each with a timestamp covering the 20 sets of timestamps
+           time_altaz is a dictionary of timestamps against alt,az positions
+           {timestamp:(alt,az), timestamp:(alt,az),....}
+           """
         timelist = list(time_altaz.keys())
         timelist.sort()
 
         curves = {}
       
-        # first five timestamps
+        # timestamp[0]                   0 seconds   -------
+        #                  30 seconds
+        # timestamp[1]                  30 seconds
+        #                  30 seconds
+        # timestamp[2]                  60 seconds    1st curve
+        #                  30 seconds
+        # timestamp[3]                  90 seconds
+        #                  30 seconds
+        # timestamp[4]                  120 seconds          ======
+        #                  30 seconds
+        # timestamp[5]                  150 seconds   ------
+        #                  30 seconds
+        # timestamp[6]                  180 seconds 
+        #                  30 seconds                        2nd curve
+        # timestamp[7]                  210 seconds
+        #                  30 seconds
+        # timestamp[8]                  240 seconds                 #########
+        #                  30 seconds
+        # timestamp[9]                  370 seconds          =======
+        #                  30 seconds
+        # timestamp[10]                 300 seconds 
+        #                  30 seconds
+        # timestamp[11]                 330 seconds                  3rd curve
+        #                  30 seconds
+        # timestamp[12]                 360 seconds 
+        #                  30 seconds
+        # timestamp[13]                 390 seconds                            ++++++++ 
+        #                  30 seconds
+        # timestamp[14]                 420 seconds                   ########
+        #                  30 seconds
+        # timestamp[15]                 450 seconds 
+        #                  30 seconds
+        # timestamp[16]                 480 seconds                             4th curve
+        #                  30 seconds
+        # timestamp[17]                 510 seconds
+        #                  30 seconds
+        # timestamp[18]                 540 seconds
+        #                  30 seconds
+        # timestamp[19]                 570 seconds                              +++++++
+
+
+
+
+        # first set of six timestamps  (5 intervals = 2.5 minutes)
         # dictionary records the curve coefficients against the first timestamp
-        curves[timelist[0]] = self.curve_maker(timelist[:5], time_altaz)
+        curves[timelist[0]] = self.curve_maker(timelist[:6], time_altaz)
 
-        # next five timestamps
-        # dictionary records the curve coefficients
-        curves[timelist[5]] = self.curve_maker(timelist[5:10], time_altaz)
+        # next curve also covers six timestamps  (5 intervals = 2.5 minutes)
+        # dictionary records the curve coefficients from timelist[4] - an overlap of 30 sec with the previous curve
+        curves[timelist[4]] = self.curve_maker(timelist[4:10], time_altaz)
 
-        # next five timestamps
-        # dictionary records the curve coefficients
-        curves[timelist[10]] = self.curve_maker(timelist[10:15], time_altaz)
+        # third curve covers seven timestamps  (6 intervals = 3 minutes)
+        # dictionary records the curve coefficients from timelist[8] - an overlap of 30 sec with the previous curve
+        curves[timelist[8]] = self.curve_maker(timelist[8:15], time_altaz)
 
-        # last five timestamps
-        # dictionary records the curve coefficients
-        curves[timelist[15]] = self.curve_maker(timelist[15:], time_altaz)
+        # last curve covers seven timestamps  (6 intervals = 3 minutes)
+        # dictionary records the curve coefficients from timelist 13 to the end, an overlap of 30 sec with the previous curve
+        curves[timelist[13]] = self.curve_maker(timelist[13:], time_altaz)
 
-        # so this last covers from timelist[15] to a further five timesamples (to timelist 20)
-        # each sample is 30 seconds, so this curve expires after a further 5*30 seconds - 2.5 minutes
+        # note the curve time coverage is less than total curve time, as the curves overlap each other
+
+        # so the curves expire at 570 seconds, 9.5 minutes, or 3 minutes after the start of the last curve
 
         return curves
 
@@ -158,43 +251,39 @@ class Telescope(object):
         "Handles the pubsub msg to move to a particular alt, az point, but then does not track"
         # disable tracking
         self.tracking = False
-        self.last_alt = self.alt
-        self.last_az = self.az
         self.alt, self.az = unpack("dd", msg['data'])
         logging.info('AltAz received ALT %s AZ %s' % (self.alt, self.az))
         self.target_name = ''
-        self.ra = None
-        self.dec = None
-        self.rconn.delete("rempi01_target_name")
-        self.rconn.delete("rempi01_target_ra")
-        self.rconn.delete("rempi01_target_dec")
-
+        self.ra = ''
+        self.dec = ''
 
 
     def target_alt_az(self, timestamp, secondcall=False):
         """Returns the wanted target alt, az at the given timestamp
            secondcall is to stop a loop occuring with the self.loadpositions function"""
         if not self.tracking:
-            # no tracking, return the static alt, az
+            # no tracking, return the static alt, az set by the altaz method (or by curve expirey)
             return self.alt, self.az
         # get the right interpolation curve for the given timestamp
-        if timestamp > self.curvetimes[3]+120:
-            # assume after two minuts, the curves has expired, another goto is required to create new curves
+        if timestamp > self.curvetimes[3]+180:
+            # assume after three minutes, the curves has expired, another goto is required to create new curves
             self.target_name = ''
-            self.ra = None
-            self.dec = None
-            self.rconn.delete("rempi01_target_name")
-            self.rconn.delete("rempi01_target_ra")
-            self.rconn.delete("rempi01_target_dec")
+            self.ra = ''
+            self.dec = ''
+            # no tracking data has been received, so assume final position measured from curve is the final altaz point.
+            popt_alt, popt_az = self.curves[self.curvetimes[3]]
             logging.error('Tracking stopped - no tracking data being received')
             self.tracking = False
+            # timestamp is set to be end of last curve
+            self.alt, self.az = alt_az_from_curve(self.curvetimes[3]+180, popt_alt, popt_az)
             return self.alt, self.az
         elif timestamp >= self.curvetimes[3]:
-            # one minute before curves expire, check if more positions have been given, if so load them
+            # three minutes before curves expire, check if more positions have been given, if so load them
             if (not secondcall) and self.loadpositions():
                 # self.curves has been updated, so call this function again
                 # but this time with secondcall True, so multiple calls to loadpositions do not occur
                 return self.target_alt_az(timestamp, True)
+            # curves have not been updates, so continue with the existing curves
             popt_alt, popt_az = self.curves[self.curvetimes[3]]
         elif timestamp >= self.curvetimes[2]:
             popt_alt, popt_az = self.curves[self.curvetimes[2]]
@@ -203,6 +292,11 @@ class Telescope(object):
         else:
            popt_alt, popt_az = self.curves[self.curvetimes[0]]
         # now get alt, az from the curve
+        return alt_az_from_curve(timestamp, popt_alt, popt_az)
+  
+
+    def alt_az_from_curve(timestamp, popt_alt, popt_az):
+        """Return alt, az from the _qcurve given timestamp and popt_alt, popt_az"""
         alt = _qcurve(timestamp, *popt_alt)
         if alt > 90.0:
             alt = 90.0
@@ -256,7 +350,7 @@ class Telescope(object):
         self.curves = self.createcurves(time_altaz)
         self.curvetimes = list(self.curves.keys())
         self.curvetimes.sort()
-        logging.info('Reading tracking data for RA %s DEC %s' % (self.ra, self.dec))
+        logging.info('Reading tracking data for RA %s DEC %s' % (positions[1], positions[2]))
         return True
 
 
@@ -265,8 +359,7 @@ class Telescope(object):
         if timestamp is None:
             targettime = datetime.utcnow()
             timestamp = targettime.replace(tzinfo=timezone.utc).timestamp()
-
-        if isinstance(timestamp, datetime):
+        elif isinstance(timestamp, datetime):
             timestamp = timestamp.replace(tzinfo=timezone.utc).timestamp()
         
         wanted_position = self.target_alt_az(timestamp)
@@ -277,22 +370,6 @@ class Telescope(object):
         self.rconn.set("rempi01_target_az", "{:1.5f}".format(wanted_position[1]))
         self.rconn.set("rempi01_target_alt_speed", "{:1.5f}".format(wanted_speed[0]))
         self.rconn.set("rempi01_target_az_speed", "{:1.5f}".format(wanted_speed[1]))
-
-        if self.target_name:
-            self.rconn.set("rempi01_target_name", self.target_name)
-        else:
-            self.rconn.delete("rempi01_target_name")
-
-        if self.ra is None:
-            self.rconn.delete("rempi01_target_ra")
-        else:
-            self.rconn.set("rempi01_target_ra", self.ra)
-
-        if self.dec is None:
-            self.rconn.delete("rempi01_target_dec")
-        else:
-            self.rconn.set("rempi01_target_dec", self.dec)
-
         return wanted_position + wanted_speed
 
 
@@ -372,35 +449,43 @@ class Telescope(object):
 
     def __call__(self): 
         "This actually runs the telescope, this is a blocking call, so run in a thread"
-        current_alt = 0
+
+        # assume initial speed is zero
         speed_alt = 0
-        current_az = 0
         speed_az = 0
 
-        # current_alt and az should eventually be taken by hardware measurement
+        # self.alt, self.az are values for a stopped scope, set by altaz method, or when tracking information
+        # has stopped. Assume they can be used as the initial value, should eventually be taken by hardware measurement
 
-        # target_* is the expected target position at the end of the time interval
-        # current_* is position at the start
+        alt = self.alt
+        az = self.az
+
+        # alt, az is the current position at the start of the time interval
+    
+        # target_alt, target_az is the expected target position at the end of the time interval
+
         # target_speed_* is the target speed at the end
         # speed_* is speed over the previous time interval
+
+        # hopefully, alt and az will become equal to target_alt, target_az at the end of the time interval
 
 
         while True:
 
-            # returns wanted target (alt, az, alt_speed, az_speed) at TIME_INTERVAL in the future
-            # and also records these target positions to redis
+            # record alt and az prior to movement over the time interval
+            last_alt = alt
+            last_az = az
 
-            self.last_alt = self.alt
-            self.last_az = self.az
-            self.alt = current_alt
-            self.az = current_az
+
+            # self.record_target returns target position and speed at TIME_INTERVAL in the future
+            # and also records these target positions to redis
 
             future_time = datetime.utcnow()+timedelta(seconds=self.TIME_INTERVAL)
             target_alt, target_az, target_speed_alt, target_speed_az = self.record_target(future_time)
 
-            # get speed for the next time interval, where target_*, target_speed_* are taken for TIME_INTERVAL time in the future
-            speed_alt = self.get_speed(current_alt, speed_alt, target_alt, target_speed_alt)
-            speed_az = self.get_speed(current_az, speed_az, target_az, target_speed_az)
+            # get speed for the next time interval, where targets are taken for TIME_INTERVAL time in the future
+            speed_alt = self.get_speed(alt, speed_alt, target_alt, target_speed_alt)
+            speed_az = self.get_speed(az, speed_az, target_az, target_speed_az)
 
             ######## call motor control with speed_alt, speed_az  ##########
 
@@ -408,31 +493,31 @@ class Telescope(object):
 
             # get new position after the time interval,
             # this will, in due course, be measured from scope sensors
-            current_alt = current_alt + speed_alt * self.TIME_INTERVAL
-            current_az = current_az + speed_az * self.TIME_INTERVAL
+            alt = alt + speed_alt * self.TIME_INTERVAL
+            az = az + speed_az * self.TIME_INTERVAL
 
-            while current_az >= 360.0:
-                current_az = current_az - 360.0
-            while current_az < 0.0:
-                current_az = current_az + 360.0
+            while az >= 360.0:
+                az = az - 360.0
+            while az < 0.0:
+                az = az + 360.0
 
-            if current_alt > 90.0:
-                current_alt = 90.0
-            if current_alt < -90.0:
-                current_alt = -90.0
+            if alt > 90.0:
+                alt = 90.0
+            if alt < -90.0:
+                alt = -90.0
 
             # set values into redis for reading by the web service, and also
             # pack timestamp,alt,az into a structure of three floats, for sending
             # to remote server 
             self.rconn.set("rempi01_current_time", future_time.strftime("%H:%M:%S.%f"))
-            self.rconn.set("rempi01_current_alt", "{:1.5f}".format(current_alt))
-            self.rconn.set("rempi01_current_az", "{:1.5f}".format(current_az))
+            self.rconn.set("rempi01_current_alt", "{:1.5f}".format(alt))
+            self.rconn.set("rempi01_current_az", "{:1.5f}".format(az))
             timestamp = future_time.replace(tzinfo=timezone.utc).timestamp()
-            self.rconn.set("telescope_position", pack("ddd", timestamp, current_alt, current_az))
+            self.rconn.set("telescope_position", pack("ddd", timestamp, alt, az))
 
             # current positions should now be equal to the previous target positions for the end of the time interval
-            error_alt = target_alt - current_alt
-            error_az = target_az - current_az
+            error_alt = target_alt - alt
+            error_az = target_az - az
             # print(error_alt, error_az)
 
 
